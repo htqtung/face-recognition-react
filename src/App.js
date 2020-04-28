7e96a3a51f619dfec6017962dc8845bab2db48a7
Tung Huynh <htqtung@gmail.com>
2020-04-28 12:34:10 +0300

    added facial recognition feature

--- src/App.js
+++ src/App.js
@@ -1,10 +1,18 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './App.css';
 import Navigation from './components/Navigation/Navigation';
 import Logo from './components/Logo/Logo';
 import ImageLinkForm from './components/ImageLinkForm/ImageLinkForm';
 import Rank from './components/Rank/Rank';
+import FaceRecognition from './components/FaceRecognition/FaceRecognition';
 import Particles from 'react-particles-js';
+import Clarifai from 'clarifai';
+
+const FACE_DETECT_KEY = '5ca4a1901d4b4757aa24bae8dd4ec97c';
+
+const app = new Clarifai.App({
+  apiKey: FACE_DETECT_KEY,
+});
 
 const particlesOptions = {
   particles: {
@@ -18,17 +26,92 @@ const particlesOptions = {
   },
 };
 
-function App() {
-  return (
-    <div className='App'>
-      <Particles params={particlesOptions} className='particles' />
-      <Navigation />
-      <Logo />
-      <Rank />
-      <ImageLinkForm />
-      {/* <FaceRecognition /> */}
-    </div>
-  );
+class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      input: '',
+      imageURL: '',
+      box: {},
+      boxes: [],
+    };
+  }
+
+  calculateFaceLocation = (data) => {
+    const clarifaiFace = data.outputs[0].data.regions[0].region_info.bounding_box;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    return {
+      leftCol: clarifaiFace.left_col * width,
+      topRow: clarifaiFace.top_row * height,
+      rightCol: width - clarifaiFace.right_col * width,
+      bottomRow: height - clarifaiFace.bottom_row * height,
+    };
+  };
+
+  calculateFaceLocations = (data) => {
+    const clarifaiFaces = data.outputs[0].data.regions;
+    const image = document.getElementById('inputImage');
+    const width = Number(image.width);
+    const height = Number(image.height);
+    const faceBoxesArray = [];
+    for (const face of clarifaiFaces) {
+      const clarifaiFace = face.region_info.bounding_box;
+      const boxObject = {
+        leftCol: clarifaiFace.left_col * width,
+        topRow: clarifaiFace.top_row * height,
+        rightCol: width - clarifaiFace.right_col * width,
+        bottomRow: height - clarifaiFace.bottom_row * height,
+      };
+      faceBoxesArray.push(boxObject);
+    }
+    return faceBoxesArray;
+  };
+
+  displayFaceBox = (box) => {
+    this.setState({ box: box });
+  };
+
+  displayFaceBoxes = (boxes) => {
+    this.setState({ boxes: boxes });
+  };
+
+  onInputChange = (event) => {
+    this.setState({
+      input: event.target.value,
+    });
+  };
+
+  onButtonSubmit = () => {
+    this.setState({ imageURL: this.state.input });
+    app.models
+      .predict(Clarifai.FACE_DETECT_MODEL, this.state.input)
+      .then((response) =>
+        this.displayFaceBoxes(this.calculateFaceLocations(response))
+      )
+      .catch((error) => console.log(error));
+  };
+
+  render() {
+    return (
+      <div className='App'>
+        <Particles params={particlesOptions} className='particles' />
+        <Navigation />
+        <Logo />
+        <Rank />
+        <ImageLinkForm
+          onInputChange={this.onInputChange}
+          onButtonSubmit={this.onButtonSubmit}
+        />
+        <FaceRecognition
+          imageURL={this.state.imageURL}
+          box={this.state.box}
+          boxes={this.state.boxes}
+        />
+      </div>
+    );
+  }
 }
 
 export default App;
