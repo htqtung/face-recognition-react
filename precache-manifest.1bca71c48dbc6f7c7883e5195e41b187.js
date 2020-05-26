8cb6b21d91bc3c3539570cda91c64f16d866759a
Tung Huynh <htqtung@gmail.com>
2020-05-26 16:45:47 +0300

    Updates

---
+++ precache-manifest.1bca71c48dbc6f7c7883e5195e41b187.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f54f47163f5e2be6e930ca8e254371e",
+    "url": "/face-recognition-react/index.html"
+  },
+  {
+    "revision": "c5ca351b638d518eeec4",
+    "url": "/face-recognition-react/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "ce74a5298553a6c75131",
+    "url": "/face-recognition-react/static/css/main.2694b6f7.chunk.css"
+  },
+  {
+    "revision": "c5ca351b638d518eeec4",
+    "url": "/face-recognition-react/static/js/2.1999c796.chunk.js"
+  },
+  {
+    "revision": "501f379103cbfcb5ff11211dd39a28a2",
+    "url": "/face-recognition-react/static/js/2.1999c796.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ce74a5298553a6c75131",
+    "url": "/face-recognition-react/static/js/main.d7528214.chunk.js"
+  },
+  {
+    "revision": "237a6e436403be351b5d",
+    "url": "/face-recognition-react/static/js/runtime-main.dcc6e335.js"
+  }
+]);
